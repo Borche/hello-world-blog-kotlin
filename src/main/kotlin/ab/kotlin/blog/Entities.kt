5889f1b8a2fb68ce2f79bc41d86d0e3300ff6baf
Andreas Börjesson <borjesson.andreas.borjesson@gmail.com>
2021-02-04 23:37:57 +0100

    Some more experiment.

--- src/main/kotlin/ab/kotlin/blog/Entities.kt
+++ src/main/kotlin/ab/kotlin/blog/Entities.kt
@@ -23,3 +23,38 @@ class User(
         var lastname: String,
         var description: String? = null,
         @Id @GeneratedValue var id: Long? = null)
+
+data class UserSmacker(
+        val id: Int,
+        val name: String,
+        val username: String,
+        val email: String,
+        val address: Address,
+        val phone: String,
+        val website: String,
+        val company: Company
+)
+
+data class Address(
+        val street: String,
+        val suite: String,
+        val city: String,
+        val zipCode: String,
+        val geo: Geo
+)
+
+data class Geo(
+        val lat: String,
+        val lng: String
+)
+
+data class Company(
+        val name: String,
+        val catchPhrase: String,
+        val bs: String
+)
+
+
+
+
+
--- src/main/kotlin/ab/kotlin/blog/HttpControllers.kt
+++ src/main/kotlin/ab/kotlin/blog/HttpControllers.kt
@@ -5,29 +5,49 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
 import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/article")
 class ArticleController(private val repository: ArticleRepository) {
 
+    /**
+     * For now, requires the trailing backslash
+     */
     @GetMapping("/")
     fun findAll() = repository.findAllByOrderByAddedAtDesc()
 
     @GetMapping("/{slug}")
     fun findOne(@PathVariable slug: String) =
             repository.findBySlug(slug) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This article does not exist")
-
+    
 }
 
 @RestController
 @RequestMapping("/api/user")
 class UserController(private val repository: UserRepository) {
 
-    @GetMapping("/")
+    @GetMapping()
     fun findAll() = repository.findAll()
 
     @GetMapping("/{login}")
     fun findOne(@PathVariable login: String) =
             repository.findByLogin(login) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This user does not exist")
+}
+
+@RestController
+@RequestMapping("/smack-that")
+class AndreasController {
+
+    @GetMapping
+    fun smackThat(): List<*>? {
+        return makeDaCall()
+    }
+
+    fun makeDaCall(): List<*>? {
+        val res = RestTemplate().getForEntity("https://jsonplaceholder.typicode.com/users", List::class.java)
+
+        return res.body
+    }
 }
